93827550635071cd727c947a2220ec223f8a633c
ElimuStuart <stuartelimu@gmail.com>
2020-03-31 11:17:16 +0300

    update static root setting

--- readit/settings.py
+++ readit/settings.py
@@ -126,7 +126,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "static_root")
 
 MEDIA_URL = '/media/'
 
